1a8522abd89e37fbd652c7ef78c76dadfc2fb172
aunnnn <aun.wirawit@gmail.com>
2018-03-01 17:53:12 +0700

    Initial Commit

---
+++ CryptoPortfolio/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CryptoPortfolio
+//
+//  Created by Wirawit Rueopas on 3/1/2561 BE.
+//  Copyright © 2561 Wirawit Rueopas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
